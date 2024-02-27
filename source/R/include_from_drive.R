cf1d2629f27e00ad85f741dcb05dad4bfd3ce85a
wlangera <ward.langeraert@inbo.be>
2024-02-27 14:19:26 +0100

    loop if multiple files with same name

--- source/R/include_from_drive.R
+++ source/R/include_from_drive.R
@@ -5,23 +5,26 @@ include_from_drive <- function(name, figures = figs, overwrite = FALSE, ...) {
   full_path <- the_fig$path
   rel_path <- fs::path_rel(full_path, common_part)
   local_path <- fs::path("media", rel_path)
-  # maak de folderstructuur als deze nog niet bestaat
-  fs::dir_create(fs::path_dir(local_path))
-  # met overwrite = FALSE, zal de functie een error geven als de file al bestaat
-  # daarom twee gevallen toelaten:
-  # 1: overwrite = TRUE
-  if (overwrite) {
-    df <- drive_download(
-      file = the_fig,
-      path = local_path,
-      overwrite = overwrite)
+
+  # loop indien meerdere bestanden met zelfde naam maar in andere folders
+  for (i in seq_along(local_path)) {
+    # maak de folderstructuur als deze nog niet bestaat
+    fs::dir_create(fs::path_dir(local_path[i]))
+    # met overwrite = FALSE, zal de functie een error geven als de file al bestaat
+    # daarom twee gevallen toelaten:
+    # 1: overwrite = TRUE
+    if (overwrite) {
+      df <- drive_download(
+        file = the_fig[i, ],
+        path = local_path[i],
+        overwrite = overwrite)
+    }
+    # 2: bestand bestaat nog niet, download het
+    if (!file.exists(local_path[i])) {
+      df <- drive_download(
+        file = the_fig[i, ],
+        path = local_path[i],
+        overwrite = overwrite)
+    }
   }
-  # 2: bestand bestaat nog niet, download het
-  if (!file.exists(local_path)) {
-    df <- drive_download(
-      file = the_fig,
-      path = local_path,
-      overwrite = overwrite)
-  }
-  knitr::include_graphics(local_path, ...)
 }
