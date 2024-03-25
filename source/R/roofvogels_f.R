3222620b468b9881a9c791374a845875f305ea1d
wlangera <ward.langeraert@inbo.be>
2024-03-25 14:17:37 +0100

    create predator function

---
+++ source/R/roofvogels_f.R
@@ -0,0 +1,9 @@
+roofvogels_f <- function() {
+  roofvogels <- c("Blauwe Kiekendief", "Boomvalk", "Bruine Kiekendief",
+                  "Buizerd", "Grauwe Kiekendief", "Ransuil", "Rode Wouw",
+                  "Slechtvalk", "Smelleken", "Sperwer", "Steenuil",
+                  "Steppekiekendief", "Torenvalk", "Velduil", "Wespendief",
+                  "Zwarte Wouw")
+
+  return(roofvogels)
+}
