f2296a1c9291671f4ccccbf37438f5e612b58241
wlangera <ward.langeraert@inbo.be>
2024-04-17 19:08:00 +0200

    setup function to transform crs

---
+++ source/targets/data_preparation/R/amersfoort_to_lambert72.R
@@ -0,0 +1,30 @@
+amersfoort_to_lambert72 <- function(sf_object) {
+  # Transform CRS according to specific pipeline
+  # see https://inbo.github.io/tutorials/tutorials/spatial_transform_crs_2/
+  pipelines <- sf_proj_pipelines("EPSG:28992", "EPSG:31370")
+
+  # We select the pipeline with lowest accuracy, by filtering on accuracy
+  chosen_pipeline_definition <- pipelines %>%
+    filter(accuracy < 0.02) %>% # choose lowest!
+    pull(definition)
+
+  # Transform according to most accurate pipeline
+  out_sf <-
+    st_transform(st_geometry(sf_object), "EPSG:31370",
+                 pipeline = chosen_pipeline_definition
+    ) %>%
+    st_sf(st_drop_geometry(sf_object), geometry = .) %>%
+    as_tibble() %>%
+    st_as_sf() %>%
+    rename(
+      x_amersfoord = x_coord,
+      y_amersfoord = y_coord
+    ) %>%
+    mutate(
+      x_lambert = unlist(map(.data$geometry, 1)),
+      y_lambert = unlist(map(.data$geometry, 2))
+    ) %>%
+    select(oid:y_amersfoord, x_lambert, y_lambert, everything())
+
+  return(out_sf)
+}
