f20544592fa8adedf49e69e3772f0d69ed163b55
wlangera <ward.langeraert@inbo.be>
2024-04-18 14:04:40 +0200

    rename functions file paths to data

--- source/targets/data_preparation/R/path_to_files.R
+++ source/targets/data_preparation/R/path_to_files.R
@@ -1,4 +1,4 @@
-# Read in raw data from SOVON
+# Path to raw data from SOVON
 path_to_counts_sovon <- function(proj_path, file) {
   file_path <- file.path(proj_path, "data", "mas", file)
   return(file_path)
