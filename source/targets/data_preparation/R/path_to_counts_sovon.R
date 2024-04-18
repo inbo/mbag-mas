b87179499be3fee71ef6d57d10360853b9609aa2
wlangera <ward.langeraert@inbo.be>
2024-04-18 14:02:50 +0200

    replace function only to return path

--- source/targets/data_preparation/R/path_to_counts_sovon.R
+++ source/targets/data_preparation/R/path_to_counts_sovon.R
@@ -1,5 +1,5 @@
 # Read in raw data from SOVON
-read_counts_sovon <- function(proj_path, file) {
+path_to_counts_sovon <- function(proj_path, file) {
   file_path <- file.path(proj_path, "data", "mas", file)
-  return(read_sf(file_path))
+  return(file_path)
 }
