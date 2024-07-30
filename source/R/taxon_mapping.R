0c3ec66db84c943460a9b409428d8f150dc2140d
wlangera <ward.langeraert@inbo.be>
2024-07-30 17:00:57 +0200

    cleanup function

--- source/R/taxon_mapping.R
+++ source/R/taxon_mapping.R
@@ -1,5 +1,5 @@
 # Function to determine the structure of the data frames in the list
-get_structure <- function(df_list) {
+get_df_structure <- function(df_list) {
   non_na_df <- df_list %>% compact() %>% first()
   empty_df <- map(non_na_df, ~ NA)
   empty_df <- as_tibble(empty_df)
@@ -7,14 +7,16 @@ get_structure <- function(df_list) {
 }
 
 # https://gist.github.com/damianooldoni/3fa9cc1ffa67377a9757df097d48d19f
-# define get_vernacular_name() core function
 # input: a vernacular name
-# output: the best matched scientific name from the GBIF Backbone, NA_character_ if no match found
-get_vernacular_name <- function(vernacular_name) {
+# output: the best matched scientific name from the GBIF Backbone
+# NA_character_ if no match found
+match_vernacular_name <- function(vernacular_name) {
   names <- rgbif::name_lookup(
     vernacular_name,
     datasetKey = "d7dddbf4-2cf0-4f39-9b2a-bb099caae36c",
-    limit = 1)$data # this returns the most likely taxon
+    limit = 1, # this returns the most likely taxon
+    rank = "SPECIES",
+    higherTaxonKey = 1)$data # Animalia
 
   if (nrow(names) > 0) {
     names
@@ -23,48 +25,50 @@ get_vernacular_name <- function(vernacular_name) {
   }
 }
 
+# Input dataframe with vernacular names and get taxon information
 map_taxa_from_vernacular <- function(
     vernacular_name_df,
-    vernacular_name_col) {
+    vernacular_name_col,
+    out_cols = "scientificName") {
 
-  n_vernacular_names_df <- vernacular_names_df %>%
+  # Match vernacular names to get taxonomic info
+  matched_names_df <- vernacular_name_df %>%
 
     # group  by vernacular name and compact the data
     group_by(.data[[vernacular_name_col]]) %>%
     nest() %>%
 
     # find scientific name for each (distinct) vernacular name
-    mutate(scientificName = map(.data[[vernacular_name_col]],
-                                get_vernacular_name))
+    mutate(taxon_df = map(.data[[vernacular_name_col]],
+                          match_vernacular_name))
 
+  # Create output dataframe
+  out_df <- matched_names_df %>%
 
-  # Determine the structure of the data frames
-  empty_df <- get_structure(n_vernacular_names_df$scientificName)
-
-  out_df <- n_vernacular_names_df %>%
-    mutate(scientificName = map(scientificName, ~ {
+    # Unnest taxon dataframes
+    mutate(taxon_df = map(.data$taxon_df, ~ {
+        # Unnesting only possible if all dataframes have the same structure
         if (all(is.na(.x))) {
-          empty_df
+          get_df_structure(matched_names_df$taxon_df)
         } else {
           .x
         }
       })
     ) %>%
-    unnest(scientificName) %>%
-    # ungroup result
+    unnest("taxon_df") %>%
     ungroup() %>%
-    # remove unneeded columns
-    select(-one_of("data")) %>%
 
-    # add other columns from input df vernacular_names_df
+    # Remove unneeded columns
+    select(-"data") %>%
+
+    # Add other columns from input df
     right_join(vernacular_name_df, by = vernacular_name_col) %>%
 
-    # set new column scientificName at the right side
-    select(all_of(names(vernacular_name_df)), scientificName) %>%
+    # Set desired column(s) at the right side
+    select(all_of(names(vernacular_name_df)), all_of(out_cols)) %>%
 
-    # reorder rows based on original order in input df
-    right_join(vernacular_name_df,
-               by = names(vernacular_name_df))
+    # Reorder rows based on original order in input df
+    right_join(vernacular_name_df, by = names(vernacular_name_df))
 
   return(out_df)
 }
