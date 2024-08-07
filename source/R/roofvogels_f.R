7b24fd10d5e6b41c592f78cc0edff7f3762c9f66
wlangera <ward.langeraert@inbo.be>
2024-08-07 09:42:01 +0200

    only signle predator function

--- source/R/roofvogels_f.R
+++
@@ -1,9 +0,0 @@
-roofvogels_f <- function() {
-  roofvogels <- c("Blauwe Kiekendief", "Boomvalk", "Bruine Kiekendief",
-                  "Buizerd", "Grauwe Kiekendief", "Ransuil", "Rode Wouw",
-                  "Slechtvalk", "Smelleken", "Sperwer", "Steenuil",
-                  "Steppekiekendief", "Torenvalk", "Velduil", "Wespendief",
-                  "Zwarte Wouw")
-
-  return(roofvogels)
-}
