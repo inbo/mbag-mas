720d6fe2304cdb3bd4a6ba9b34753b500df43018
wlangera <ward.langeraert@inbo.be>
2024-08-07 10:54:01 +0200

    do not make row names

--- source/R/tar_read_sf.R
+++ source/R/tar_read_sf.R
@@ -1,4 +1,12 @@
 tar_read_sf <- function(name, store) {
-  do.call(rbind.data.frame,
-          tar_read_raw(name = name, store = store))
+  # Get target data
+  list_sf <- tar_read_raw(name = name, store = store)
+
+  # Create dataframe
+  out_df <- do.call(
+    what = rbind.data.frame,
+    args = c(list_sf, make.row.names = FALSE)
+    )
+
+  return(out_df)
 }
