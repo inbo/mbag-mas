7ecfdda909a5347475c92274ca739c59654cdf1f
wlangera <ward.langeraert@inbo.be>
2024-08-12 13:48:35 +0200

    dwc mapping of static and unchanged values

--- source/targets/data_preparation/R/dwc_mapping.R
+++ source/targets/data_preparation/R/dwc_mapping.R
@@ -1,4 +1,5 @@
-spatial_mapping_df <- function(data_sf) {
+# Convert Lambert to decimal degrees
+spatial_mapping <- function(data_sf) {
   require("dplyr")
   require("rlang")
   require("sf")
@@ -32,8 +33,63 @@ spatial_mapping_df <- function(data_sf) {
   return(out_df)
 }
 
+# Static values are used for Darwin Core terms that need the same value for all
+# records. They are usually missing from the input data.
+static_mapping <- function(data_sf) {
+  require("dplyr")
 
+  out_df <- data_sf %>%
+    mutate(
+      dwc_type                 = "??",
+      dwc_datasetID            = "??",
+      dwc_language             = "en",
+      dwc_license              = paste0("http://creativecommons.org/",
+                                        "publicdomain/zero/1.0/"),
+      dwc_publisher            = paste0("Research Institute for Nature and",
+                                        "Forest (INBO)"),
+      dwc_rightsHolder         = "INBO",
+      dwc_accessRights         = "http://www.inbo.be/en/norms-for-data-use",
+      dwc_institutionCode      = "INBO",
+      dwc_datasetName          = "??",
+      dwc_ownerInstitutionCode = "INBO",
+      dwc_collectionCode       = "MAS",
+      dwc_kingdom              = "Animalia",
+      dwc_nomenclaturalCode    = "ICZN",
+      dwc_eventType            = "Survey",
+      dwc_samplingProtocol     = "??",
+      dwc_samplingEffort       = "??",
+      dwc_continent            = "Europe",
+      dwc_country              = "Belgium",
+      dwc_stateProvince        = "Flanders",
+      dwc_countryCode          = "BE",
+      dwc_basisOfRecord        = "HumanObservation"
+    )
 
+  return(out_df)
+}
+
+# Unchanged values are used for Darwin Core terms whose content is an exact
+# copy of the corresponding field in the input data.
+unchanged_mapping <- function(data_sf) {
+  require("dplyr")
+
+  out_df <- data_sf %>%
+    rename(
+      "dwc_vernacularName"    = "raw_naam",
+      "dwc_eventDate"         = "raw_datum",
+      "dwc_year"              = "raw_jaar",
+      "dwc_month"             = "raw_maand",
+      "dwc_day"               = "raw_dag",
+      "dwc_recordedBy"        = "raw_waarnemer",
+      "dwc_individualCount"   = "raw_aantal",
+      "dwc_locationID"        = "raw_plotnaam",
+      "dwc_varbatimBehavior"  = "raw_wrntype_omschrijving",
+      "dwc_occurrenceRemarks" = "raw_opmerk",
+      "dwc_taxonID"           = "raw_soortnr"
+    )
+
+  return(out_df)
+}
 
 # Darwin Core mapping function
 dwc_mapping <- function(data_sf) {
@@ -53,8 +109,13 @@ dwc_mapping <- function(data_sf) {
   colnames(raw_data) <- paste0("raw_", colnames(raw_data))
   sf::st_geometry(raw_data) <- "raw_geometry"
 
-
   # Spatial processing
-  spatial_df <- spatial_mapping_df(raw_data)
+  spatial_df <- spatial_mapping(raw_data)
+
+  # Static DwC mapping
+  static_df <- static_mapping(spatial_df)
+
+  # Unchanged DwC mapping
+  unchanged_df <- unchanged_mapping(static_df)
 
 }
