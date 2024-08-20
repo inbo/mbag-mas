9808f9e5cfe2205ba2f5cb28e6a83be88bf81610
wlangera <ward.langeraert@inbo.be>
2024-08-20 13:28:14 +0200

    add weight column

--- source/R/geocomputations.R
+++ source/R/geocomputations.R
@@ -11,11 +11,10 @@
 #'
 #' @examples
 point_to_gridcell <- function(
-  xy,
-  cell_width_m = 500,
-  point_position = c("center", "lowerleft",
-                     "upperleft", "lowerright", "upperright"),
-  crs = 31370) {
+    xy,
+    cell_width_m = 500,
+    point_position = c("center", "lowerleft", "upperleft", "lowerright", "upperright"),
+    crs = 31370) {
   point_position <- match.arg(point_position)
 
   if (point_position != "center") stop(point_position, " not yet implemented")
@@ -30,8 +29,8 @@ point_to_gridcell <- function(
                              nQuadSegs = 1)
 
   # rotate 45 degrees around centroid
-  rot <- function(a) matrix(c(cos(a), sin(a), - sin(a), cos(a)), 2, 2)
-  pl <- (xy_buffer - xy) * rot(pi / 4) + xy
+  rot <- function(a) matrix(c(cos(a), sin(a), -sin(a), cos(a)), 2, 2)
+  pl <- (xy_buffer - xy) * rot(pi/4) + xy
   pl <- sf::st_sf(data.frame(xy_df, pl), crs = crs)
   return(pl)
 }
@@ -41,8 +40,7 @@ point_to_gridcell <- function(
 #'
 #' @param grid_cell A polygon within which boundaries zonal statistics will be
 #' calculated
-#' @param layer A rasterlayer containing land use classes or
-#' a polygon layer (sf object)
+#' @param layer A rasterlayer containing land use classes or a polygon layer (sf object)
 #' @param grid_group_by_col A character vector of columns to group by for zones
 #' @param layer_group_by_col A character vector of columns to group by for
 #' layer
@@ -52,14 +50,15 @@ point_to_gridcell <- function(
 #'
 #' @examples
 landusemetrics_grid_cell <- function(
-  grid_cell,
-  layer,
-  grid_group_by_col = "POINT_ID",
-  layer_group_by_col = "",
-  progress = FALSE
+    grid_cell,
+    layer,
+    grid_group_by_col = "POINT_ID",
+    layer_group_by_col = "",
+    weight_col = NULL,
+    progress = FALSE
 ) {
   require(duckdb)
-  if (inherits(layer, "SpatRaster") || inherits(layer, "RasterLayer")) {
+  if (inherits(layer, "SpatRaster") | inherits(layer, "RasterLayer")) {
     crs_grid <- gsub("^((.*?),\\n\\s*?){2}", "", sf::st_crs(grid_cell)$wkt)
     crs_layer <- gsub("^((.*?),\\n\\s*?){2}", "", terra::crs(layer))
     assertthat::assert_that(crs_grid == crs_layer)
@@ -101,14 +100,27 @@ landusemetrics_grid_cell <- function(
       inner_join(cell_areas, by = grid_group_by_col) %>%
       arrow::write_dataset(path = temparrow)
 
-    int <- arrow::open_dataset(temparrow) %>%
-      arrow::to_duckdb() %>%
-      group_by(!!!syms(grid_group_by_col),
-               !!!syms(layer_group_by_col),
-               cell_area) %>%
-      summarise(area_m2 = sum(area)) %>%
-      mutate(area_prop = area_m2 / cell_area) %>%
-      collect()
+    if (is.null(weight_col)) {
+      int <- arrow::open_dataset(temparrow) %>%
+        arrow::to_duckdb() %>%
+        group_by(!!!syms(grid_group_by_col),
+                 !!!syms(layer_group_by_col),
+                 cell_area) %>%
+        summarise(area_m2 = sum(area)) %>%
+        mutate(area_prop = area_m2 / cell_area) %>%
+        collect()
+    } else {
+      int <- arrow::open_dataset(temparrow) %>%
+        arrow::to_duckdb() %>%
+        mutate(area_w = area * !!sym(weight_col)) %>%
+        group_by(!!!syms(grid_group_by_col),
+                 !!!syms(layer_group_by_col),
+                 cell_area) %>%
+        summarise(area_m2 = sum(area_w)) %>%
+        mutate(area_prop = area_m2 / cell_area) %>%
+        collect()
+    }
+
 
     return(int)
   }
