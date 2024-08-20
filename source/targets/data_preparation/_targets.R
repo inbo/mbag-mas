9541892a3d9eb677d8abde022fc6bd5865f88bde
wlangera <ward.langeraert@inbo.be>
2024-08-20 14:53:07 +0200

    use sample

--- source/targets/data_preparation/_targets.R
+++ source/targets/data_preparation/_targets.R
@@ -305,11 +305,11 @@ list(
   tar_target(
     name = add_crop_prop_var,
     command = calc_crop_prop_by_year(
-      punten_df = add_other_bo_var,
+      punten_df = sample_by_year,
       crop_layer = crop_layers_by_year,
       group_by_col = "GWSNAM_H"
     ),
-    pattern = map(add_other_bo_var, crop_layers_by_year),
+    pattern = map(sample_by_year, crop_layers_by_year),
     iteration = "list"
   )
 )
