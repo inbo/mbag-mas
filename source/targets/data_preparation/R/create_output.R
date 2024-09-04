104c42b3f412e74c4f1a7e6a6d251a0ac30be91e
Ward Langeraert <ward.langeraert@inbo.be>
2024-09-04 13:55:47 +0200

    function to create gpkg output

--- source/targets/data_preparation/R/create_output.R
+++ source/targets/data_preparation/R/create_output.R
@@ -20,3 +20,27 @@ create_output_csv <- function(x, file, path, suffix_by = NA, ...) {
   # Return file path
   return(full_path)
 }
+
+create_output_gpkg <- function(x, file, path, suffix_by = NA, ...) {
+  # Add suffix if provided
+  if (!is.na(suffix_by)) {
+    suffix <- paste(
+      unique(c(min(st_drop_geometry(x)[suffix_by]),
+               max(st_drop_geometry(x)[suffix_by]))),
+      collapse = "_"
+    )
+
+    full_path <- paste0(file.path(path, file), "_", suffix, ".gpkg")
+  } else {
+    full_path <- paste0(file.path(path, file), ".gpkg")
+  }
+
+  # Create output path
+  dir.create(path, showWarnings = FALSE, recursive = TRUE)
+
+  # Write out dataframe
+  sf::st_write(x, dsn = full_path, ...)
+
+  # Return file path
+  return(full_path)
+}
