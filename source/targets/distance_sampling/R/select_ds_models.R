9edd128357aa222ba898e99c3e2a206d9ff1308e
Ward Langeraert <ward.langeraert@inbo.be>
2025-08-08 12:20:03 +0200

    resolve ties

--- source/targets/distance_sampling/R/select_ds_models.R
+++ source/targets/distance_sampling/R/select_ds_models.R
@@ -5,7 +5,10 @@ select_ds_models <- function(aic_diff, model_list, aic_tol = 2) {
   selected_model <- aic_diff %>%
     arrange("`Delta AIC`") %>%
     filter(.data$`Delta AIC` <= aic_tol) %>%
+    # Lowest number of parameters within tolerance
     slice_min(.data$params, n = 1) %>%
+    # In case of ties, choose lowest AIC
+    slice_min(.data$`Delta AIC`, n = 1) %>%
     pull("Model")
 
   return(model_list[[selected_model]])
