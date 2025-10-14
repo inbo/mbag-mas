f6a6479a6606360f6d94583efe59d1d3071a6ca8
Ward Langeraert <ward.langeraert@inbo.be>
2025-10-14 17:09:47 +0200

    check linters

--- source/bookdown/mas_uitrol_vlaanderen/scripts/plot_remote_sensing.R
+++ source/bookdown/mas_uitrol_vlaanderen/scripts/plot_remote_sensing.R
@@ -16,13 +16,17 @@
 #' scale.
 #' @param legend_shape Character string for the legend title of the shape scale.
 plot_time_series <- function(
-    df,
-    loc_name,
-    colour_var,
-    shape_var = NULL,
-    legend_colour = "Legende",
-    legend_shape = "Legende"
+  df,
+  loc_name,
+  colour_var,
+  shape_var = NULL,
+  legend_colour = "Legende",
+  legend_shape = "Legende"
 ) {
+  require("dplyr")
+  require("ggplot2")
+  require("rlang")
+
   # Rules for different indicator types
   if ("ndvi" %in% names(df)) {
     y_axis_title <- "NDVI"
@@ -37,11 +41,12 @@ plot_time_series <- function(
   # Prepare data
   data <- df %>%
     # Get plot
-    filter(pointid %in% loc_name) %>%
+    filter(.data$pointid %in% loc_name) %>%
     # Create variable to set x-axis limits
     mutate(
       monthday_date = as.Date(
-        paste0("2000-", substr(monthday, 1, 2), "-", substr(monthday, 3, 4))
+        paste0("2000-", substr(.data$monthday, 1, 2), "-",
+               substr(.data$monthday, 3, 4))
       )
     )
 
@@ -49,10 +54,12 @@ plot_time_series <- function(
   if (is.null(shape_var)) {
     # Create output
     p <- data %>%
-      ggplot(aes(x = monthday_date, y = !!sym(y_var), group = ref_id)) +
+      ggplot(aes(x = .data$monthday_date, y = !!sym(y_var),
+                 group = .data$ref_id)) +
       # Create time series
       geom_line(alpha = 0.5, linetype = 5) +
-      geom_point(aes(colour = !!sym(colour_var)), size = 2, show.legend = TRUE) +
+      geom_point(aes(colour = !!sym(colour_var)), size = 2,
+                 show.legend = TRUE) +
       # Add reference lines
       geom_hline(yintercept = cutoff, linetype = "dotdash", colour = "black") +
       # Settings
@@ -66,7 +73,8 @@ plot_time_series <- function(
 
   # Return complex plot
   data %>%
-    ggplot(aes(x = monthday_date, y = !!sym(y_var), group = ref_id)) +
+    ggplot(aes(x = .data$monthday_date, y = !!sym(y_var),
+               group = .data$ref_id)) +
     # Create time series
     geom_line(alpha = 0.5, linetype = 5) +
     geom_point(aes(colour = !!sym(colour_var), shape = !!sym(shape_var)),
@@ -74,7 +82,8 @@ plot_time_series <- function(
     # Add reference lines
     geom_hline(yintercept = cutoff, linetype = "dotdash", colour = "black") +
     # Settings
-    labs(x = "", y = y_axis_title, colour = legend_colour, shape = legend_shape) +
+    labs(x = "", y = y_axis_title, colour = legend_colour,
+         shape = legend_shape) +
     scale_x_date(date_breaks = "1 month", date_labels = "%b",
                  limits = as.Date(c("2000-02-01", "2000-09-01"))) +
     scale_shape_manual(
@@ -95,15 +104,19 @@ plot_time_series <- function(
 #' @param df A data frame containing columns for `year`, `gwsgrp_h`
 #' (crop group), `trend`, and `indicator`.
 plot_trend_by_crop <- function(df) {
+  require("dplyr")
+  require("ggplot2")
+  require("rlang")
+
   legend_fill <- paste0(
     "Trend in bedekking (", toupper(unique(df$indicator)), ")"
   )
 
   df %>%
-    group_by(year, gwsgrp_h) %>%
+    group_by(.data$year, .data$gwsgrp_h) %>%
     mutate(n = n()) %>%
     ungroup() %>%
-    ggplot(aes(x = gwsgrp_h, fill = trend)) +
+    ggplot(aes(x = .data$gwsgrp_h, fill = .data$trend)) +
     geom_bar(position = "fill") +
     geom_text(aes(label = n, y = 1.05), size = 2.3) +
     labs(x = "", y = "Proportie percelen", fill = legend_fill) +
@@ -111,7 +124,7 @@ plot_trend_by_crop <- function(df) {
     theme_minimal(base_size = 12) +
     theme(axis.text.x = element_text(angle = 75, hjust = 1),
           strip.text = element_text(face = "bold"),
-          legend.position = c(0.8, -0.4),
+          legend.position = c(0.8, -0.42),
           legend.background = element_rect(fill = "white", color = "darkgrey"),
           legend.margin = margin(6, 6, 6, 6),
           legend.box = "horizontal",
