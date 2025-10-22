b8ba54eaf10dd6c5763e0f127f59c5171e93ff15
Ward Langeraert <ward.langeraert@inbo.be>
2025-10-22 11:02:50 +0200

    update colums to split datasets

--- source/targets/data_preparation/R/split_dwc_event_occ.R
+++ source/targets/data_preparation/R/split_dwc_event_occ.R
@@ -1,10 +1,10 @@
 split_dwc_event_occ <- function(df) {
-  # Event file: metadata + event + location
+  # Darwin Core terms suitable for the events file
   event_fields <- c(
+    "eventID", "parentEventID", "eventType",
     "type", "language", "license", "publisher", "rightsHolder", "accessRights",
-    "collectionCode", "institutionCode",
-    "eventID", "parentEventID", "eventType", "samplingProtocol",
-    "samplingEffort", "eventDate", "year", "month", "day",
+    "samplingProtocol", "samplingEffort",
+    "eventDate", "year", "month", "day",
     "continent", "country", "countryCode", "stateProvince", "locationID",
     "verbatimLatitude", "verbatimLongitude", "verbatimCoordinateSystem",
     "verbatimSRS", "decimalLatitude", "decimalLongitude",
@@ -13,20 +13,9 @@ split_dwc_event_occ <- function(df) {
     "informationWithheld", "dataGeneralizations", "georeferenceRemarks"
   )
 
-  # Occurrence file: metadata + event + occurrence + identification + taxonomy
-  occ_fields <- c(
-    "type", "language", "license", "publisher", "rightsHolder", "accessRights",
-    "collectionCode", "institutionCode",
-    "eventID", "occurrenceID", "basisOfRecord",
-    "recordedBy", "organismQuantity", "organismQuantityType", "lifeStage",
-    "occurrenceStatus", "behavior", "verbatimBehavior",
-    "occurrenceRemarks",
-    "identifiedBy", "identificationVerificationStatus", "vernacularName",
-    "taxonID", "scientificName", "scientificNameAuthorship",
-    "scientificNameID", "taxonRank", "nomenclaturalCode",
-    "verbatimIdentification", "identificationQualifier", "verbatimTaxonRank",
-    "kingdom", "phylum", "class", "order", "family", "genus", "species"
-  )
+  # All remaining DwC terms go to occurrence file
+  # Include eventID again (to interlink event and occurrence)
+  occ_fields <- setdiff(names(df), setdiff(event_fields, "eventID"))
 
   # Select existing columns
   events_df <- df[, intersect(event_fields, names(df)), drop = FALSE]
