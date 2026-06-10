d1962471fb03f566c5492da54c108b07cb8dbcbc
Ward Langeraert <ward.langeraert@inbo.be>
2026-06-10 15:01:15 +0200

    read branch per species

--- source/bookdown/power_mas_light/R/tar_read_mas.R
+++ source/bookdown/power_mas_light/R/tar_read_mas.R
@@ -1,6 +1,15 @@
-tar_read_mas <- function(...) {
+tar_read_mas <- function(name, species = NULL, ...) {
+  # Targets store
   mbag_dir <- rprojroot::find_root_file(criterion = rprojroot::is_git_root)
   store_path <- file.path(mbag_dir, "source", "targets", "power_mas_light",
                           "_targets")
-  targets::tar_read(..., store = store_path)
+
+  # Specify name
+  if (!is.null(species)) {
+    species <- gsub("\\s", ".", species)
+    name <- paste0(name, paste0("_", species))
+  }
+
+  # Get target
+  targets::tar_read_raw(name = name, ..., store = store_path)
 }
