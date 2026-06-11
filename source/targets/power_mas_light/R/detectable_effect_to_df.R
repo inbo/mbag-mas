9f2192f1b0ec49bc661ee5d875bde4cce9ce6667
Ward Langeraert <ward.langeraert@inbo.be>
2026-06-11 09:59:20 +0200

    calculate percentages

--- source/targets/power_mas_light/R/detectable_effect_to_df.R
+++ source/targets/power_mas_light/R/detectable_effect_to_df.R
@@ -14,12 +14,17 @@ detectable_effect_to_df <- function(x) {
       data.frame(
         species = species,
         branch = branch,
-        detectable_effect_low = value[2],
-        detectable_effect = value[1],
-        detectable_effect_high = value[3],
+        beta3_low = value[2],
+        beta3 = value[1],
+        beta3_high = value[3],
         as.data.frame(attr(value, "design")),
         check.names = FALSE
-      )
+      ) %>%
+        mutate(
+          effect_pct = 100 * (exp(beta3) - 1),
+          effect_pct_low = 100 * (exp(beta3_low) - 1),
+          effect_pct_high = 100 * (exp(beta3_high) - 1)
+        )
     }
   )
 
