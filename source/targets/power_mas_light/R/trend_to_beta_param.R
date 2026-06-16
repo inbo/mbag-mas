673c88fd34f5bb35142defadcecb53479585c19a
Ward Langeraert <ward.langeraert@inbo.be>
2026-06-16 14:18:24 +0200

    fix linters

--- source/targets/power_mas_light/R/trend_to_beta_param.R
+++ source/targets/power_mas_light/R/trend_to_beta_param.R
@@ -1,7 +1,7 @@
 trend_to_beta_param <- function(
-    trend_buiten,
-    verschil_binnen = 0.02,
-    min_trend_binnen = 1.01
+  trend_buiten,
+  verschil_binnen = 0.02,
+  min_trend_binnen = 1.01
   ) {
   if (trend_buiten + verschil_binnen <= min_trend_binnen) {
     return(log(min_trend_binnen / trend_buiten))
