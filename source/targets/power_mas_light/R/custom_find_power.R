c8d30c4ef244811133caabaaaa5137d645972020
Ward Langeraert <ward.langeraert@inbo.be>
2026-06-25 09:12:59 +0200

    improve power function with seed

---
+++ source/targets/power_mas_light/R/custom_find_power.R
@@ -0,0 +1,18 @@
+custom_find_power <- function(
+  design,
+  design_digits,
+  ...,
+  db_file,
+  seed = NULL
+) {
+  require(designpower)
+
+  if (!is.null(seed)) set.seed(seed)
+
+  designpower::find_power(
+    design = design[-which(names(design) == "tar_group")],
+    design_digits = design_digits,
+    ...
+    filename = db_file
+  )
+}
