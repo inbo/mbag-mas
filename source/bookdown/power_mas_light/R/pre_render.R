3e88f8ad7024df8856ee43a52abb6baeac7ed0fc
Ward Langeraert <ward.langeraert@inbo.be>
2026-06-26 08:10:39 +0200

    fix linters

--- source/bookdown/power_mas_light/R/pre_render.R
+++ source/bookdown/power_mas_light/R/pre_render.R
@@ -1,4 +1,4 @@
-# remotes::install_github("inbo/flandersqmd#57a604d981d85d632db65c98b1abb2e9d032359c")
+# remotes::install_github("inbo/flandersqmd#57a604d981d85d632db65c98b1abb2e9d032359c") # nolint: commented_code_linter, line_length_linter
 library(flandersqmd)
 library(targets)
 source("R/tar_read_mas.R")
--- source/bookdown/power_mas_light/_extensions/inbo/flandersqmd-book/filters/post_render.R
+++ source/bookdown/power_mas_light/_extensions/inbo/flandersqmd-book/filters/post_render.R
@@ -1,4 +1,4 @@
-# remotes::install_github("inbo/flandersqmd#7")
+# remotes::install_github("inbo/flandersqmd#57a604d981d85d632db65c98b1abb2e9d032359c") # nolint: commented_code_linter, line_length_linter
 library(flandersqmd)
 
 message <- c(
