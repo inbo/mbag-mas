eee58034b00480273d97ecbb533eb65588e28f9f
Ward Langeraert <ward.langeraert@inbo.be>
2026-06-30 17:55:44 +0200

    improve resolution and axis title cover photo

--- source/bookdown/power_mas_light/figures/create_cover.R
+++ source/bookdown/power_mas_light/figures/create_cover.R
@@ -3,6 +3,8 @@ library(targets)
 library(magick)
 library(ggplot2)
 library(INBOtheme)
+library(grid)
+library(ragg)
 
 theme_set(theme_inbo(base_size = 12, transparent = TRUE))
 conflicted::conflicts_prefer(dplyr::filter)
@@ -17,7 +19,30 @@ targets_path <- file.path(mbag_dir, "source", "targets", "power_mas_light")
 design_list <- tar_read_mas("design_list", branch = 1)[[1]]
 source(file.path(targets_path, "R", "simulate_mas_data.R"))
 
+# ------------------------------------------------------------------
+# Resolution scaling factor
+# ------------------------------------------------------------------
+
+scale <- 4
+
+# ------------------------------------------------------------------
 # Capture plot
+# ------------------------------------------------------------------
+designpower::find_power(
+  design = design_list$design[
+    -which(names(design_list$design) == "tar_group")
+  ],
+  design_digits = design_list$digits,
+  opti = "beta_3",
+  sim_power = simulate_mas_data,
+  power = 0.9,
+  alpha = 0.1,
+  filename = file.path(
+    targets_path,
+    "power_mas_light_akkervogel2.duckdb"
+  )
+)
+
 p <- grDevices::recordPlot({
   designpower::find_power(
     design = design_list$design[
@@ -28,75 +53,120 @@ p <- grDevices::recordPlot({
     sim_power = simulate_mas_data,
     power = 0.9,
     alpha = 0.1,
-    filename = file.path(targets_path, "power_mas_light_akkervogel2.duckdb")
+    filename = file.path(
+      targets_path,
+      "power_mas_light_akkervogel2.duckdb"
+    )
   )
 })
 
-# Save recorded plot
-png("./figures/cover_raw.png", width = 800, height = 400)
+# ------------------------------------------------------------------
+# Save at high resolution
+# ------------------------------------------------------------------
+
+agg_png(
+  "./figures/cover_raw.png",
+  width = 800 * scale,
+  height = 400 * scale,
+  units = "px",
+  res = 96 * scale,
+  background = "white"
+)
+
 p
 dev.off()
 
-# Cut image
+# ------------------------------------------------------------------
+# Remove middle strip
+# ------------------------------------------------------------------
+
 img <- image_read("./figures/cover_raw.png")
 info <- image_info(img)
 
-# Parameters
-cut_start <- 70    # x-coordinate where the cut starts
-cut_width <- 125   # width of the strip to remove
+cut_start <- 90 * scale
+cut_width <- 128 * scale
 
-# Left part
 left <- image_crop(
   img,
-  geometry = sprintf("%dx%d+0+0",
-                     cut_start,
-                     info$height)
+  geometry = sprintf(
+    "%dx%d+0+0",
+    cut_start,
+    info$height
+  )
 )
 
-# Right part
 right <- image_crop(
   img,
-  geometry = sprintf("%dx%d+%d+0",
-                     info$width - cut_start - cut_width,
-                     info$height,
-                     cut_start + cut_width)
+  geometry = sprintf(
+    "%dx%d+%d+0",
+    info$width - cut_start - cut_width,
+    info$height,
+    cut_start + cut_width
+  )
 )
 
-# Stitch them together horizontally
 merged <- image_append(c(left, right), stack = FALSE)
 
-# Crop 100 pixels from the right
 info_merged <- image_info(merged)
+
+# ------------------------------------------------------------------
+# Remove legend right
+# ------------------------------------------------------------------
+
 cropped <- image_crop(
   merged,
-  geometry = sprintf("%dx%d+0+0", info_merged$width - 90, info_merged$height)
+  geometry = sprintf(
+    "%dx%d+0+0",
+    info_merged$width - 120 * scale,
+    info_merged$height
+  )
 )
 
-# Save final cover image
-image_write(cropped, "./figures/cover.png")
+image_write(cropped, "./figures/cover_en.png")
 
-# Get figure
-img <- image_read("./figures/cover.png")
+# ------------------------------------------------------------------
+# Replace axis titles
+# ------------------------------------------------------------------
+
+img <- image_read("./figures/cover_en.png")
 img <- image_draw(img)
 
-# Replace old y-axis text with white rectangle
 rect(
-  xleft = 5,
-  ybottom = 80,
-  xright = 25,
-  ytop = 320,
+  xleft = 5 * scale,
+  ybottom = 80 * scale,
+  xright = 30 * scale,
+  ytop = 320 * scale,
   col = "white",
   border = NA
 )
 
-# New text
-grid::grid.text(
+grid.text(
   "Geschatte power",
-  x = unit(15, "pt"),
+  x = unit(16 * scale, "pt"),
   y = unit(0.53, "npc"),
   rot = 90,
-  gp = grid::gpar(
-    fontsize = 12,
+  gp = gpar(
+    fontsize = 15 * scale,
+    fontfamily = "sans",
+    col = "black"
+  )
+)
+
+rect(
+  xleft = 200 * scale,
+  xright = 300 * scale,
+  ybottom = 370 * scale,
+  ytop = 392 * scale,
+  col = "white",
+  border = NA
+)
+
+grid.text(
+  expression(beta[3]~"(effectgrootte)"),
+  x = unit(0.55, "npc"),
+  y = unit(0.05, "npc"),
+  gp = gpar(
+    fontsize = 15 * scale,
     fontfamily = "sans",
     col = "black"
   )
@@ -104,4 +174,4 @@ grid::grid.text(
 
 dev.off()
 
-image_write(img, "./figures/cover_nl.png")
+image_write(img, "./figures/cover.png")
